b36a5c1fcc834eda216d09623619ac4d84c71c5f
Vishwas G <vishwasg@thoughtworks.com>
2021-12-01 10:51:41 +0530

    First star

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,12 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    var largerMeasurement : Int = 0
+    var previousNumber = input[0].toInt()
+    for (i in input){
+        val number = i.toInt()
+        if(previousNumber<number)
+            largerMeasurement++
+        previousNumber=number
+    }
+    println(largerMeasurement)
 }
