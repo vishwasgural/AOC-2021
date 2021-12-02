42e8eb9e0bd50d6dcfc176c0c40faf6aff055d86
Vishwas G <vishwasg@thoughtworks.com>
2021-12-02 12:51:27 +0530

    day 2 2nd star

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -6,17 +6,20 @@ fun main() {
     val input = readInput("day2/Day02")
     var depth = 0
     var forward = 0
+    var aim = 0
     for (i in input) {
         var moment = i.split(' ')
-        if (moment[0] == "forward")
+        if (moment[0] == "forward"){
             forward += moment[1].toInt()
-        else if(moment[0] == "down")
-            depth += moment[1].toInt()
+            var temp = moment[1].toInt() * aim
+            depth += temp
+        }
+        else if(moment[0] == "down") {
+            aim += moment[1].toInt()}
 
-        else if(moment[0] == "up")
-            depth -= moment[1].toInt()
+        else if(moment[0] == "up") {
+            aim -= moment[1].toInt()
+        }
     }
-    println(depth)
-    println(forward)
     println(depth * forward)
 }
